7800f281d60fce1d0beea3b29614e29fd4c55071
abraham314 <raulabraham.nieto@gmail.com>
2020-06-16 18:10:24 -0500

    ss

--- app.py
+++ app.py
@@ -65,11 +65,6 @@ app.layout = html.Div(children=[
 
 app = dash.Dash()
 
-app.layout =html.Div([
-    html.H1("Mapa"),
-    #html.Iframe(id="map",srcDoc=open("covid_por_mpio.html",encoding="utf8").read(),width="100%",height="600")
-        
-    ])
 
 if __name__ == '__main__':
     app.run_server()
